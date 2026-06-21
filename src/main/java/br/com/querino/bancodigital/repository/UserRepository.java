017bc5be946237b774154c64c17558eae4d427e6
Querino <caioquerino04@gmail.com>
2026-06-21 17:18:01 -0300

    feat(repository): adiciona métodos para verificação de existência e busca de usuário por CPF, email e telefone

--- src/main/java/br/com/querino/bancodigital/repository/UserRepository.java
+++ src/main/java/br/com/querino/bancodigital/repository/UserRepository.java
@@ -1,9 +1,15 @@
 package br.com.querino.bancodigital.repository;
 
 import java.util.UUID;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.querino.bancodigital.entity.UserEntity;
 
-public interface UserRepository extends JpaRepository<UserEntity, UUID> {}
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+    boolean existsByCpf(String cpf);
+    boolean existsByEmail(String email);
+    boolean existsByPhone(String phone);
+    Optional<UserEntity> findByEmail(String email);
+}
