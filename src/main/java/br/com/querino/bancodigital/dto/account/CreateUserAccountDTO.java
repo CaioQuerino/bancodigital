1f18aa6d00c96823b5df714c82b93ccb389e0fae
Querino <caioquerino04@gmail.com>
2026-06-22 23:26:59 -0300

    docs(swagger): adiciona documentação OpenAPI ao CreateUserAccountDTO
    - Adiciona anotação @Schema na classe CreateUserAccountDTO
    - Documenta o cadastro conjunto de usuário e conta bancária
    - Documenta o objeto user com informações do titular
    - Documenta o objeto account com informações da conta
    - Mantém validações de obrigatoriedade e validação aninhada
    - Melhora a visualização da requisição no Swagger UI
    - Padroniza a documentação dos DTOs do módulo de contas

--- src/main/java/br/com/querino/bancodigital/dto/account/CreateUserAccountDTO.java
+++ src/main/java/br/com/querino/bancodigital/dto/account/CreateUserAccountDTO.java
@@ -1,6 +1,7 @@
 package br.com.querino.bancodigital.dto.account;
 
 import br.com.querino.bancodigital.dto.user.CreateUserDTO;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -12,12 +13,25 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(
+    name = "CreateUserAccountDTO",
+    description = "DTO responsável pelo cadastro simultâneo de usuário e conta bancária."
+)
 public class CreateUserAccountDTO {
+
     @Valid
     @NotNull(message = "Dados do usuário são obrigatórios")
+    @Schema(
+        description = "Informações do titular da conta.",
+        requiredMode = Schema.RequiredMode.REQUIRED
+    )
     private CreateUserDTO user;
 
     @Valid
     @NotNull(message = "Dados da conta são obrigatórios")
+    @Schema(
+        description = "Informações da conta bancária a ser criada.",
+        requiredMode = Schema.RequiredMode.REQUIRED
+    )
     private CreateAccountDTO account;
 }
