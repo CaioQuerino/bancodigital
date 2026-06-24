4519a514f286b5aef73d3bbfa5bf284f5d92fc28
Querino <caioquerino04@gmail.com>
2026-06-23 23:54:38 -0300

    docs(repository): adiciona JavaDoc ao TransferRepository
    * documenta responsabilidade do repositório de transferências
    * descreve operações de persistência da entidade Transfer
    * melhora legibilidade e manutenção do código
    * não altera comportamento da aplicação

--- src/main/java/br/com/querino/bancodigital/repository/TransferRepository.java
+++ src/main/java/br/com/querino/bancodigital/repository/TransferRepository.java
@@ -6,4 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.querino.bancodigital.entity.Transfer;
 
+/**
+ * Repositório responsável pelas operações de persistência
+ * das transferências bancárias.
+ */
 public interface TransferRepository extends JpaRepository<Transfer, UUID> {}
