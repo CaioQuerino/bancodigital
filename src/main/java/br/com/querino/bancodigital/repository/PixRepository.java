908d9126ff2d9c8531cd41e0b9231e9a31e17842
Querino <caioquerino04@gmail.com>
2026-06-24 00:01:23 -0300

    docs(swagger): documentar repositório de chaves PIX
    - Adiciona JavaDoc à interface PixRepository
    - Documenta método findByKeyPix()
    - Documenta método existsByKeyPix()
    - Melhora entendimento das operações de persistência PIX
    - Padroniza documentação dos repositórios

--- src/main/java/br/com/querino/bancodigital/repository/PixRepository.java
+++ src/main/java/br/com/querino/bancodigital/repository/PixRepository.java
@@ -7,7 +7,25 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.querino.bancodigital.entity.PixEntity;
 
+/**
+ * Repositório responsável pelas operações de persistência
+ * relacionadas às chaves PIX.
+ */
 public interface PixRepository extends JpaRepository<PixEntity, UUID> {
+
+    /**
+     * Busca uma chave PIX pelo valor informado.
+     *
+     * @param keyPix valor da chave PIX
+     * @return chave encontrada, caso exista
+     */
     Optional<PixEntity> findByKeyPix(String keyPix);
+
+    /**
+     * Verifica se uma chave PIX já está cadastrada.
+     *
+     * @param keyPix valor da chave PIX
+     * @return true se existir, false caso contrário
+     */
     boolean existsByKeyPix(String keyPix);
 }
