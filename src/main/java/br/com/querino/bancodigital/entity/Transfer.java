fe7062b273e127b21bd352e56c30137d4c994fb0
Querino <caioquerino04@gmail.com>
2026-06-23 23:57:27 -0300

    feat(swagger): documenta entidade Transfer com OpenAPI
    * adiciona @Schema à entidade Transfer
    * documenta finalidade da entidade de transferências
    * documenta conta de origem da transferência
    * documenta conta de destino da transferência
    * define relacionamentos como somente leitura na documentação
    * melhora visualização do modelo de transferência no Swagger UI

--- src/main/java/br/com/querino/bancodigital/entity/Transfer.java
+++ src/main/java/br/com/querino/bancodigital/entity/Transfer.java
@@ -1,5 +1,6 @@
 package br.com.querino.bancodigital.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -17,13 +18,25 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(
+        name = "Transferência",
+        description = "Entidade responsável por representar transferências entre contas bancárias"
+)
 public class Transfer extends Transaction {
 
-	@ManyToOne
-	@JoinColumn(name = "origin_account_id", nullable = false)
-	private AccountEntity originAccount;
+    @ManyToOne
+    @JoinColumn(name = "origin_account_id", nullable = false)
+    @Schema(
+            description = "Conta de origem da transferência",
+            accessMode = Schema.AccessMode.READ_ONLY
+    )
+    private AccountEntity originAccount;
 
-	@ManyToOne
-	@JoinColumn(name = "destination_account_id", nullable = false)
-	private AccountEntity destinationAccount;
+    @ManyToOne
+    @JoinColumn(name = "destination_account_id", nullable = false)
+    @Schema(
+            description = "Conta de destino da transferência",
+            accessMode = Schema.AccessMode.READ_ONLY
+    )
+    private AccountEntity destinationAccount;
 }
